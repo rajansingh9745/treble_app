14fc078e16a152e2ea9e461ea2fe2832490d6901
Pierre-Hugues Husson <phh@phh.me>
2024-02-21 23:21:01 +0100

    Add an option to disable surfaceflinger's gl backpressure for smoother ui

--- app/src/main/java/me/phh/treble/app/MiscSettings.kt
+++ app/src/main/java/me/phh/treble/app/MiscSettings.kt
@@ -45,6 +45,7 @@ object MiscSettings : Settings {
     val linearBrightness = "key_misc_linear_brightness"
     val forceDisplay5g = "key_misc_force_display_5g"
     val disableVoiceCallIn = "key_misc_disable_voice_call_in"
+    val disableSfGlBackpressure = "key_misc_disable_sf_gl_backpressure"
 
     override fun enabled() = true
 }
